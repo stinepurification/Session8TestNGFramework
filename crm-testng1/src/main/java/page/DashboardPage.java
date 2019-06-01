9e3ca42bed7ad83b99e3c5428ccd71a5a4fa3e23
Stine <Stine@DESKTOP-91B7HJ8>
2019-06-01 13:53:30 -0500

    Updated with not line in between.

--- crm-testng1/src/main/java/page/DashboardPage.java
+++ crm-testng1/src/main/java/page/DashboardPage.java
@@ -1,5 +1,22 @@
 package page;
 
-public class DashboardPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
+public class DashboardPage {
+	WebDriver driver;
+
+	public DashboardPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	@FindBy(how = How.XPATH, using = "//h2[text()='Dashboard ']")
+	WebElement PageTitle;
+	@FindBy(how = How.LINK_TEXT, using = "Add Deposit")
+	WebElement AddDepositButton;
+
+	public void clickOnAddDepositButton() {
+		AddDepositButton.click();
+	}
 }
