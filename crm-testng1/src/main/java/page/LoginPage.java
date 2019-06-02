8ebd33013264cc6fc649789970bc87a4456faf3a
Rajib <Rajib@Rajib-Samsung>
2019-06-02 03:17:16 -0500

    Rajib Eusofzai
    Added 1 New Page and Test to Verify Login.
    Added 1 New Page and Test to create and verify a New Transaction.

--- crm-testng1/src/main/java/page/LoginPage.java
+++ crm-testng1/src/main/java/page/LoginPage.java
@@ -6,19 +6,19 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class LoginPage {
-	WebDriver driver; //Global
-	//Every Page must have a constructor to invite the driver 
+	WebDriver driver; // Global
+	// Every Page must have a constructor to invite the driver
+
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
 	}
-	
-	//Element Library
+
+	// Element Library
 	@FindBy(how = How.ID, using = "username") WebElement UserName;
 	@FindBy(how = How.ID, using = "password") WebElement Password;
-	@FindBy(how = How.NAME, using = "ogin") WebElement SignInButton;
-	//Methods to interact with the elements
+	@FindBy(how = How.NAME, using = "login") WebElement SignInButton;
 	
-	public void login(String userName, String password) {	
+	public void login(String userName, String password) {
 		UserName.sendKeys(userName);
 		Password.sendKeys(password);
 		SignInButton.click();
